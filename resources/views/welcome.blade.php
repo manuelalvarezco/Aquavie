597fc4f92c277bfe4a8a0a2c2312bd3c0a7754c8
manuel alvarez <alvarezbautista.luis@gmail.com>
2020-03-26 22:45:30 -0500

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,91 @@
-@extends('layouts.app')
-@section('content')
+    @extends('layouts.app')
+    @section('content')
 
-    <div class="main mt-5">
-        <div class="container" style="margin-top:5.5em">
+        <div class="main mt-5">
+            <div class="container" style="margin-top:5.5em">
 
-            <div class="row mb-4">
-                <div class="col-sm-12 col-md-6 info">
-                    <h1 style="text-transform: uppercase; color: white">En la corporación Aqua Vie te ayudamos a optimizar tus impuestos por medio de la siembra de árboles</h1>
-                    <a href="/contactus" class=" btn btn-outline-warning" data-original-title="" title="">QUIERO MÁS INFORMACIÓN</a>    
+                <div class="row mb-4">
+                    <div class="col-sm-12 col-md-6 info">
+                        <h1 style="text-transform: uppercase; color: white">En la corporación Aqua Vie te ayudamos a optimizar tus impuestos por medio de la siembra de árboles</h1>
+                        <a href="/contact" class=" btn btn-outline-warning" data-original-title="" title="">QUIERO MÁS INFORMACIÓN</a>    
+                    </div>
+                    
                 </div>
                 
             </div>
-            
         </div>
-    </div>
-    <div class="container">
-        <div class="aliados mt-4">
-            <div class="row aliados d-flex align-items-center" style="margin-top: 50px"> 
+        <div class="container">
+            <div class="aliados mt-4">
+                <div class="row aliados d-flex align-items-center" style="margin-top: 50px"> 
+                    <div class="col-md-4">
+                    <img width="250" src="{{url('images/aliado01.png')}}" alt="aliado">
+                </div>
                 <div class="col-md-4">
-                <img width="250" src="{{url('images/aliado01.png')}}" alt="aliado">
+                    <img width="250" src="{{url('images/aliado02.png')}}" alt="aliado">
+                </div>
+                <div class="col-md-4"> 
+                    <img width="250" src="{{url('images/aliado03.png')}}" alt="aliado">
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4">
-                <img width="250" src="{{url('images/aliado02.png')}}" alt="aliado">
+
+        </div>
+        <div class="container">
+            <div class="row prueba-2">
+                <div class="col-md-7">
+                    <div class="home-program-intro">
+                        <h2>Siembra con nosotros</h2>
+                        <h3>Lorem inpsup</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
+                        </div>
+                    <!-- .home-program-intro -->
+                </div>
+                <div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
+                    <div class="home-program-links">
+                        <a href="#"><span>Más información</span></a>
+                    </div>
+                    <!-- .home-program-links -->
+                </div>
             </div>
-            <div class="col-md-4"> 
-                <img width="250" src="{{url('images/aliado03.png')}}" alt="aliado">
+        </div>
+        <div class="container">
+            <div class="row prueba-2">
+                <div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
+                    <div class="home-program-links">
+                        <a href="#"><span>Más información</span></a>
+                    </div>
+                    <!-- .home-program-links -->
+                </div>
+                <div class="col-md-7">
+                    <div class="home-program-intro">
+                        <h2>Calcula tu huella</h2>
+                        <h3>Lorem inpsup</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
+                        </div>
+                    <!-- .home-program-intro -->
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row prueba-2">
+                <div class="col-md-7">
+                    <div class="home-program-intro">
+                        <h2>Contáctanos</h2>
+                        <h3>Lorem inpsup</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
+                        </div>
+                    <!-- .home-program-intro -->
+                </div>
+                <div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
+                    <div class="home-program-links">
+                        <a href="#"><span>Escríbenos para mas información</span></a>
+                    </div>
+                    <!-- .home-program-links -->
                 </div>
             </div>
         </div>
 
-    </div>
-    <div class="container">
-        <div class="row prueba-2">
-			<div class="col-md-7">
-				<div class="home-program-intro">
-					<h2>Siembra con nosotros</h2>
-					<h3>Lorem inpsup</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
-                    </div>
-				<!-- .home-program-intro -->
-			</div>
-			<div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
-				<div class="home-program-links">
-					<a href="/online/realworld/"><span>Más información</span></a>
-				</div>
-				<!-- .home-program-links -->
-			</div>
-		</div>
-    </div>
-    <div class="container">
-        <div class="row prueba-2">
-            <div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
-				<div class="home-program-links">
-					<a href="/online/realworld/"><span>Más información</span></a>
-				</div>
-				<!-- .home-program-links -->
-			</div>
-			<div class="col-md-7">
-				<div class="home-program-intro">
-					<h2>Calcula tu huella</h2>
-					<h3>Lorem inpsup</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
-                    </div>
-				<!-- .home-program-intro -->
-			</div>
-			
-		</div>
-    </div>
 
-    <div class="container">
-        <div class="row prueba-2">
-			<div class="col-md-7">
-				<div class="home-program-intro">
-					<h2>Contáctanos</h2>
-					<h3>Lorem inpsup</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veritatis eveniet, illo quae expedita nam consectetur et eius, recusandae reprehenderit eligendi dolore reiciendis accusantium, harum ipsum ratione aut! Tenetur, ex.</p>
-                    </div>
-				<!-- .home-program-intro -->
-			</div>
-			<div class=" prueba col-md-5" style="background-image: url(https://images.unsplash.com/photo-1567147136203-9e71087d6027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);">
-				<div class="home-program-links">
-					<a href="/online/realworld/"><span>Escríbenos para mas información</span></a>
-				</div>
-				<!-- .home-program-links -->
-			</div>
-		</div>
-    </div>
-
-
-@endsection
+    @endsection
